60789cdc8178bf038be678e0141640e7944a013b
bekozi <benkoziol@gmail.com>
2016-01-09 22:26:02 -0700

    wip: minor

--- pyugrid/flexible_mesh/logging_ugrid.py
+++ pyugrid/flexible_mesh/logging_ugrid.py
@@ -3,8 +3,8 @@ import logging
 
 from pyugrid.flexible_mesh.mpi import MPI_RANK
 
-# level = logging.ERROR
-level = logging.DEBUG
+level = logging.ERROR
+# level = logging.DEBUG
 
 log = logging.getLogger('pyugrid')
 log.parent = None
@@ -13,14 +13,12 @@ formatter = logging.Formatter(fmt='[%(asctime)s] %(levelname)s: %(name)s: %(mess
 console = logging.StreamHandler()
 console.setLevel(level)
 console.setFormatter(formatter)
-
-#tdk: remove
-fh = logging.FileHandler('/tmp/pyugrid.log', mode='w')
-fh.setFormatter(formatter)
-fh.setLevel(level)
-
 log.addHandler(console)
-log.addHandler(fh)
+
+# fh = logging.FileHandler('/tmp/pyugrid.log', mode='w')
+# fh.setFormatter(formatter)
+# fh.setLevel(level)
+# log.addHandler(fh)
 
 
 class log_entry_exit(object):
